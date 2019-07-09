317ac4d38f4d2121036be7fbd97739c19b9f0ef8
Bruno Kamada <brunokamada@neppoadm.com.br>
2019-07-08 22:16:42 -0300

    schedule page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,13 +1,55 @@
 import React, { Component } from 'react'
 require('../style/institutionRegister.css');
 import Navbar from './navbar'
+require('../style/home.css');
+import Calendar from 'react-calendar';
+import Modal from './Modal/Modal';
 
 class InstitutionRegister extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            date: new Date(),
+            isShowing: false
+        }
+    }
+
+    onChange = date => this.setState({ date })
+
+    openModalHandler = () => {
+        this.setState({
+            isShowing: true
+        });
+    }
+
+    closeModalHandler = () => {
+        this.setState({
+            isShowing: false
+        });
+    }
+
     render() {
         return (
             <div className="container">
                 <Navbar />
-                <h1>Home</h1>
+                <div className="header-schedule">
+                    Agenda
+                </div>
+                <div className="content-schedule">
+                    <Calendar 
+                        onChange={ this.onChange } 
+                        value={ this.state.date }
+                        onClickDay={this.openModalHandler}
+                        className="schedule"
+                    />
+                </div>
+                <Modal
+                    className="modal"
+                    show={this.state.isShowing}
+                    close={this.closeModalHandler}>
+                </Modal>
             </div>
         );
     }
